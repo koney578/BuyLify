6fbf070698170f19d67ad5fd8b4d57e798413d4a
Mateusz Horczak <mfawel@gmail.com>
2023-10-27 22:37:45 +0200

    init frontend app

---
+++ VueApp/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <body class="bg-emerald-200 min-h-screen">
+  <div class="items-center flex">
+    <slot/>
+  </div>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
