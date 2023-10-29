4a9c02d2365801036f611677c2929586bca7cd4e
Mateusz Horczak <mfawel@gmail.com>
2023-10-29 20:23:59 +0100

    home page and login page mockup

--- VueApp/pages/index.vue
+++ VueApp/pages/index.vue
@@ -3,6 +3,19 @@
 </script>
 
 <template>
-  <h1 class="color-amber-600 font-bold text-2xl">No dzien dobry uguem</h1>
-  <p>sfas</p>
+  <div class="flex items-center h-lg">
+    <div class="w-1/2 ml-10rem">
+      <h1 class="color-amber-600 font-bold text-4xl">Wyszukaj coś dla siebie!</h1>
+      <p class="color-amber-600 font-bold text-2xl">U nas najlepszy wybor!</p>
+      <label class="">
+        <input class="bg-white color-black p-1rem rounded-3xl w-2xl mt-2rem shadow-2xl" value="" placeholder="Wpisz cos sobie byku">
+      </label>
+    </div>
+    <div class="w-1/2 mt-5rem">
+<!--      <p class="color-black">prawa</p>-->
+      <img src="../images/i-just-wanna-shop.png"  alt="I just wanna Shop" class="color-fuchsia w-3/4"/>
+    </div>
+  </div>
+
+
 </template>
