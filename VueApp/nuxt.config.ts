571eb0f57fbbd89d9f5a6bdffa769ac1866c5b33
Mateusz Horczak <mfawel@gmail.com>
2023-10-29 20:23:59 +0100

    home page and login page mockup

--- VueApp/nuxt.config.ts
+++ VueApp/nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-      '@unocss/nuxt',
-      '@nuxt/ui',
-  ],
+    ssr: false,
+    devtools: {enabled: true},
+    modules: [
+        '@unocss/nuxt',
+        '@nuxt/ui',
+    ],
 })
