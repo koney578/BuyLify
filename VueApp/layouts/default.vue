a0be7494fda53a64ebb1447b848caa85c379e8d4
Mateusz Horczak <mfawel@gmail.com>
2023-10-29 23:31:28 +0100

    login, register, user settings, home page

--- VueApp/layouts/default.vue
+++ VueApp/layouts/default.vue
@@ -9,13 +9,13 @@
       <p class="color-black text-3rem font-semibold italic">BuyLify</p>
       <div class="flex w-full">
         <ul class="flex ml-2rem w-1/2">
-          <li class="color-black p-4 ml-1rem mr-1rem text-2rem">bla bla</li>
-          <li class="color-black p-4 ml-1rem mr-1rem text-2rem">bla bla</li>
+          <li class="color-black p-4 ml-1rem mr-1rem text-2rem"><a href="/">Strona Główna</a></li>
+          <li class="color-black p-4 ml-1rem mr-1rem text-2rem"><a href="/profile">Ustawienia konta</a></li>
           <li class="color-black p-4 ml-1rem mr-1rem text-2rem">bla bla</li>
         </ul>
         <ul class="flex w-1/2 justify-end">
-          <li class="color-black p-4 ml-1rem mr-1rem text-2rem">bla bla</li>
-          <li class="color-black p-4 ml-1rem mr-1rem text-2rem">bla bla</li>
+          <li class="color-black p-4 ml-1rem mr-1rem text-2rem"><a href="/login">Zaloguj się</a></li>
+          <li class="color-black p-4 ml-1rem mr-1rem text-2rem"><a href="/register">Załóż Konto</a></li>
         </ul>
       </div>
 
