56fbcb1cd200da2ce2dbe082609060b36a7abe09
Mateusz Horczak <mfawel@gmail.com>
2023-10-30 22:36:11 +0100

    button to home page

--- VueApp/pages/index.vue
+++ VueApp/pages/index.vue
@@ -4,12 +4,18 @@
 
 <template>
   <div class="flex items-center h-lg">
-    <div class="w-1/2 ml-10rem">
+    <div class="w-1/2 ml-10rem relative">
       <h1 class="color-amber-600 font-bold text-4xl">Wyszukaj coś dla siebie!</h1>
       <p class="color-amber-600 font-bold text-2xl">U nas najlepszy wybor!</p>
-      <label class="">
+      <label class="relative">
         <input class="bg-white color-black p-1rem rounded-3xl w-2xl mt-2rem shadow-2xl" value="" placeholder="Wpisz cos sobie byku">
       </label>
+      <div class="w-2xl">
+        <button type="submit"
+                class="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-0.5rem ml-auto">
+          Wyszukaj
+        </button>
+      </div>
     </div>
     <div class="w-1/2 mt-5rem">
 <!--      <p class="color-black">prawa</p>-->
