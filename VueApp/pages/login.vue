d8ddde10df6288881dd5e65a5cd8cd6096b179d6
Mateusz Horczak <mfawel@gmail.com>
2023-11-09 13:43:26 +0100

    animation, logos

--- VueApp/pages/login.vue
+++ VueApp/pages/login.vue
@@ -24,7 +24,7 @@ const login = async () => {
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      <img class="mx-auto h-auto w-1/2" src="../images/logo-text.png"
            alt="Your Company"/>
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Zaloguj się</h2>
     </div>
--- VueApp/pages/register.vue
+++ VueApp/pages/register.vue
@@ -31,7 +31,7 @@ const register = async () => {
 
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      <img class="mx-auto h-auto w-1/2" src="../images/logo-text.png"
            alt="Your Company"/>
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Zarejestruj się</h2>
     </div>
