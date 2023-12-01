aa9a9020c762ea4660260567adcabfc63c270bb4
Mateusz Horczak <mfawel@gmail.com>
2023-12-01 18:16:11 +0100

    validators in authentication

--- VueApp/src/pages/register.vue
+++ VueApp/src/pages/register.vue
@@ -7,6 +7,84 @@ const user = reactive({
   password2: ''
 })
 
+
+let passwordError = "";
+let passwordMatchError = "";
+let emailError = "";
+let usernameError = "";
+
+function validatePasswordMatch() {
+  if (user.password !== "" || user.password2 !== "") {
+    if (user.password !== user.password2) {
+      passwordMatchError = "Hasła nie są takie same";
+      return false;
+    } else {
+      passwordMatchError = "";
+      return true;
+    }
+  } else {
+    passwordMatchError = "";
+  }
+}
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function validateEmail() {
+  if (user.email !== "") {
+    if (!emailRegex.test(user.email) ) {
+      emailError = "Nieprawidłowy adres e-mail";
+      return false;
+    } else {
+      emailError = "";
+      return true;
+    }
+  } else {
+    emailError = "";
+  }
+}
+
+
+function validatePassword() {
+  if (user.password !== "") {
+    if (user.password.length < 8) {
+      passwordError = "Hasło musi mieć co najmniej 8 znaków.";
+      return false;
+    } else if (!/[A-Z]/.test(user.password)) {
+      passwordError = "Hasło musi zawierać co najmniej jedną dużą literę.";
+      return false;
+    } else if (!/\d/.test(user.password)) {
+      passwordError = "Hasło musi zawierać co najmniej jedną cyfrę.";
+      return false;
+    } else {
+      passwordError = "";
+      return true;
+    }
+  } else {
+    passwordError = "";
+  }
+}
+
+function validateUsername() {
+  if (user.username !== "") {
+    if (user.username.length < 5) {
+      usernameError = "Nazwa musi składać się przynajmniej z 5 znaków";
+      return false;
+    } else {
+      usernameError = "";
+      return true;
+    }
+  } else {
+    usernameError = ""
+  }
+}
+
+watch(user, () => {
+  validatePassword();
+  validatePasswordMatch();
+  validateEmail();
+  validateUsername();
+});
+
+
 const register = async () => {
 
   if (!user.username || !user.email || !user.password || !user.password2) {
@@ -14,10 +92,27 @@ const register = async () => {
     return
   }
 
-  if (user.password !== user.password2) {
-    console.error('Hasło i powtórzone hasło nie są takie same')
+  if (validatePasswordMatch()) {
+    console.error(passwordMatchError)
     return
   }
+
+  if (!validatePassword()) {
+    console.error(passwordError)
+    return
+  }
+
+  if (!validateEmail()) {
+    console.error(emailError)
+    return
+  }
+
+  if (!validateUsername()) {
+    console.error(usernameError)
+    return
+  }
+
+
   const router = useRouter()
   const auth = useAuthStore()
 
@@ -43,6 +138,9 @@ const register = async () => {
         <div>
           <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nazwa użytkownika</label>
           <div class="mt-2">
+            <div v-if="usernameError" class="font-semibold text-rose-600">
+              {{ usernameError }}
+            </div>
             <input v-model="user.username" id="username" name="username" type="text" autocomplete="" required=""
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-0.5rem"/>
           </div>
@@ -51,6 +149,9 @@ const register = async () => {
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Adres email</label>
           <div class="mt-2">
+            <div v-if="emailError" class="font-semibold text-rose-600">
+              {{ emailError }}
+            </div>
             <input v-model="user.email" id="email" name="email" type="email" autocomplete="email" required=""
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-0.5rem"/>
           </div>
@@ -62,6 +163,9 @@ const register = async () => {
 
           </div>
           <div class="mt-2">
+            <div v-if="passwordError" class="font-semibold text-rose-600">
+              {{ passwordError }}
+            </div>
             <input v-model="user.password" id="password" name="password" type="password" autocomplete="new-password" required=""
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-0.5rem"/>
           </div>
@@ -73,6 +177,9 @@ const register = async () => {
 
           </div>
           <div class="mt-2">
+            <div v-if="passwordMatchError" class="font-semibold text-rose-600">
+              {{ passwordMatchError }}
+            </div>
             <input v-model="user.password2" id="password2" name="password2" type="password" autocomplete="new-password" required=""
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-0.5rem"/>
           </div>
