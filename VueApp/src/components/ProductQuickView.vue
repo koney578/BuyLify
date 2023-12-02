9ba7cecc05184707f8d2c634e120e2cd26aa94bb
Mateusz Horczak <mfawel@gmail.com>
2023-12-02 18:49:08 +0100

    .

--- VueApp/src/components/ProductQuickView.vue
+++ VueApp/src/components/ProductQuickView.vue
@@ -24,18 +24,16 @@ interface Product {
   description: string,
   category: Category,
   createdAt: Date,
-  isOpen: Boolean,
+  closeModal: Function,
 }
 
 const props = defineProps<Product>()
 
-const open = ref(props.isOpen)
-
 </script>
 
 <template>
-  <TransitionRoot as="template" :show="open">
-    <Dialog as="div" class="relative z-10" @close="open = false">
+  <TransitionRoot as="template" :show=true>
+    <Dialog as="div" class="relative z-10">
       <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block" />
       </TransitionChild>
@@ -45,10 +43,11 @@ const open = ref(props.isOpen)
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 md:translate-y-0 md:scale-95" enter-to="opacity-100 translate-y-0 md:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 md:scale-100" leave-to="opacity-0 translate-y-4 md:translate-y-0 md:scale-95">
             <DialogPanel class="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
               <div class="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-<!--                <button type="button" class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8" @click="open = false">-->
-<!--                  <span class="sr-only">Close</span>-->
-<!--                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />-->
-<!--                </button> TODO jak klikniesz przycisk to potem nic sie nie otwiera, naprawic; brak animacji zamykania -->
+                <button type="button" class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8" @click="props.closeModal()">
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                </button>
+<!--                TODO jak klikniesz przycisk to potem nic sie nie otwiera, naprawic; brak animacji zamykania -->
 
                 <div class="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
                   <div class="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
--- VueApp/src/pages/board.vue
+++ VueApp/src/pages/board.vue
@@ -47,7 +47,6 @@ interface Product {
   description: string,
   category: Category,
   createdAt: Date,
-  isOpen: Boolean,
 }
 
 const defaultProduct = {
@@ -58,7 +57,6 @@ const defaultProduct = {
   description: "",
   category: noCategory,
   createdAt: new Date(),
-  isOpen: false,
 }
 
 const isProductDetailsOpen = ref(false)
@@ -67,13 +65,11 @@ const selectedProduct: Ref<Product> = ref<Product>(defaultProduct)
 
 const showProductDetails = (product: Product) => {
   selectedProduct.value = product
-  selectedProduct.value.isOpen = true
   isProductDetailsOpen.value = true
 }
 
 const closeProductDetails = () => {
   isProductDetailsOpen.value = false
-  selectedProduct.value.isOpen = true
 }
 
 
@@ -193,6 +189,7 @@ const filterPosts = async () => {
                         :description="selectedProduct.description"
                         :created-at="selectedProduct.createdAt"
                         :is-open="selectedProduct.isOpen"
+                        :closeModal="closeProductDetails"
       />
     </div>
   </div>
