34f8f5787d57954b9c5843b414e559ca6dc870c9
Krystian Borys <krystian.borys231@gmail.com>
2023-12-07 23:11:20 +0100

    added admin methods for Discount, OrderStatus and NotificationType

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/discount/DiscountController.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/discount/DiscountController.java
@@ -1,9 +1,8 @@
 package com.buylify.buylifyapp.discount;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,23 @@ public class DiscountController {
     @GetMapping
     public List<Discount> getAllDiscounts() {
         return discountService.getAllDiscounts();
+    }
+
+    @PreAuthorize("hasAuthority('administrator')")
+    @PostMapping
+    public void addDiscount(Discount discount) {
+        discountService.addDiscount(discount);
+    }
+
+    @PreAuthorize("hasAuthority('administrator')")
+    @DeleteMapping("/{id}")
+    public void deleteDiscount(@PathVariable("id") Long id) {
+        discountService.deleteDiscount(id);
+    }
+
+    @PreAuthorize("hasAuthority('administrator')")
+    @PutMapping("/{id}")
+    public void editDiscount(@PathVariable("id") Long id, Discount discountToEdit) {
+        discountService.editDiscount(id, discountToEdit);
     }
 }
