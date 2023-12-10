8563fb27e6623e7ee5efa3d2eec375ba10924447
kubacim3 <kubacim3@wp.pl>
2023-12-10 18:43:59 +0100

    Let only admin send notifications

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/notification/NotificationController.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/notification/NotificationController.java
@@ -2,6 +2,7 @@ package com.buylify.buylifyapp.notification;
 
 import com.buylify.buylifyapp.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @PreAuthorize("hasAuthority('administrator')")
     @PostMapping
     public void addNotification(@RequestBody CreateNotificationDto createNotificationDto) {
         notificationService.addNotification(createNotificationDto);
