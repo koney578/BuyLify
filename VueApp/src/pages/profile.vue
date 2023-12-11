4932717a11042c26e66d71c0c0bbf7fc41287cdc
Mateusz Horczak <mfawel@gmail.com>
2023-12-11 18:58:04 +0100

    profile frontend

--- VueApp/src/pages/profile.vue
+++ VueApp/src/pages/profile.vue
@@ -4,12 +4,72 @@ import {useAuthStore} from "~/stores/auth";
 
 const auth = useAuthStore()
 
+const user = reactive({
+  username: '',
+  // description: '',  TODO obecnie user nie posiada opisu, mozna dodac w przyszlosci, bo jest juz template
+  name: '',
+  surname: '',
+  email: '',
+  phoneNumber: '',
+})
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function validateEmail() {
+  return emailRegex.test(user.email);
+}
+
+function validatePhoneNumber() {
+  return user.phoneNumber.length === 9;
+}
+
+function validateUsername() {
+  return user.username > 5;
+}
+
+const rejectChanges = () => {
+  const router = useRouter()
+  router.push('/')
+}
+
+const editProfile = async () => {
+  if (user.username === '' || !validateUsername()) {
+    user.username = auth.user.username
+  }
+
+  if (user.name === '') {
+    user.name = auth.user.name
+  }
+
+  if (user.surname === '') {
+    user.surname = auth.user.surname
+  }
+
+  if (user.email === '' || !validateEmail()) {
+    user.email = auth.user.email
+  }
+
+  if (user.phoneNumber === '' || !validatePhoneNumber()) {
+    user.phoneNumber = auth.user.phoneNumber
+  }
+
+  const router = useRouter()
+
+  const data = await $fetch('http://localhost:8080/api/???????????????', {
+    method: 'POST',
+    body: user,
+    headers: {Authorization: 'Bearer ' + auth.token}
+  }).catch(err => console.error(err.data))
+
+  await router.push('/')
+
+
+}
 
 </script>
 
 <template>
   <div class="w-2/3 m-auto mt-2rem">
-    <form>
+    <form @submit.prevent="editProfile">
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
           <h2 class="text-base font-semibold leading-7 text-gray-900">Profil</h2>
@@ -22,7 +82,7 @@ const auth = useAuthStore()
                 <div
                     class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <span class="flex select-none items-center px-2 text-gray-500 sm:text-sm">buylify.com/</span>
-                  <input type="text" name="username" id="username" autocomplete="username"
+                  <input v-model="user.username" type="text" name="username" id="username" autocomplete="username"
                          class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-white"
                          :placeholder=auth.user.username />
                 </div>
@@ -32,7 +92,7 @@ const auth = useAuthStore()
             <div class="col-span-full">
               <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Opis</label>
               <div class="mt-2">
-              <textarea id="about" name="about" rows="3"
+              <textarea id="about" name="about" rows="3" placeholder="To nie działa, tylko na pokaz, mozna w przyszlosci podłączyć"
                         class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"/>
               </div>
               <p class="mt-3 text-sm leading-6 text-gray-600">Napisz kilka zdań o sobie.</p>
@@ -49,7 +109,7 @@ const auth = useAuthStore()
             <div class="sm:col-span-3">
               <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Imię</label>
               <div class="mt-2">
-                <input type="text" name="first-name" id="first-name" autocomplete="name"
+                <input v-model="user.name" type="text" name="first-name" id="first-name" autocomplete="name"
                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"
                       :placeholder="auth.user.name"
                 />
@@ -59,7 +119,7 @@ const auth = useAuthStore()
             <div class="sm:col-span-3">
               <label for="surname" class="block text-sm font-medium leading-6 text-gray-900">Nazwisko</label>
               <div class="mt-2">
-                <input type="text" name="surname" id="surname" autocomplete="surname"
+                <input v-model="user.surname" type="text" name="surname" id="surname" autocomplete="surname"
                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"
                       :placeholder="auth.user.surname"
                 />
@@ -69,7 +129,7 @@ const auth = useAuthStore()
             <div class="sm:col-span-3">
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Adres email</label>
               <div class="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email"
+                <input v-model="user.email" id="email" name="email" type="email" autocomplete="email"
                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"
                        :placeholder="auth.user.email"
                 />
@@ -79,7 +139,7 @@ const auth = useAuthStore()
             <div class="sm:col-span-3">
               <label for="phoneNumber" class="block text-sm font-medium leading-6 text-gray-900">Numer telefonu</label>
               <div class="mt-2">
-                <input type="text" name="phoneNumber" id="phoneNumber" autocomplete="phoneNumber"
+                <input v-model="user.phoneNumber" type="text" name="phoneNumber" id="phoneNumber" autocomplete="phoneNumber"
                        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"
                        :placeholder="auth.user.phoneNumber"
                 />
@@ -161,10 +221,10 @@ const auth = useAuthStore()
       </div>
 
       <div class="mt-6 flex items-center justify-end gap-x-6 mb-2rem">
-        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+        <button type="button" @click="rejectChanges" class="text-sm font-semibold leading-6 text-gray-900">Odrzuć</button>
         <button type="submit"
                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Save
+          Zapisz
         </button>
       </div>
     </form>
