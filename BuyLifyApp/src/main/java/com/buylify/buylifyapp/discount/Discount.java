b2fd28c2ccb3f8330a3a47ecc9ba2edca26c8d09
kubacim3 <kubacim3@wp.pl>
2023-12-14 22:52:49 +0100

    Apply discount while buying a product, reduce product count on buy

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/discount/Discount.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/discount/Discount.java
@@ -1,6 +1,8 @@
 package com.buylify.buylifyapp.discount;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,8 @@ public class Discount {
     private Long id;
 
     @NotNull
+    @Min(value = 0)
+    @Max(value = 1)
     private float discountPercent;
 
     @Column(nullable = false)
