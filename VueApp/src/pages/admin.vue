dba9d1044e6868b618c61b4bd9deec9845468e81
Mateusz Horczak <mfawel@gmail.com>
2023-12-18 00:30:42 +0100

    start auction fix

--- VueApp/src/pages/admin.vue
+++ VueApp/src/pages/admin.vue
@@ -363,7 +363,7 @@ const addNotificationType = async () => {
             <input v-model="newNotificationType.name" id="notification-name" name="notification-name" type="text"
                    autocomplete="notification-name"
                    required=""
-                   placeholder="typ powiadomienia - bez backendu nie zadziala"
+                   placeholder="typ powiadomienia"
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-0.5rem"/>
           </div>
         </div>
