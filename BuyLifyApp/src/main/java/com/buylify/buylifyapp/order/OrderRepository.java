cd17c6a6b18af7bb441b70a12f83098c65ad1a09
rutrob <faherlele@gmail.com>
2024-03-25 10:33:23 +0100

    Ignore products with quantity of 0 while fetching

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/order/OrderRepository.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/order/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.buylify.buylifyapp.order;
 
+import com.buylify.buylifyapp.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
