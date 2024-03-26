5bb15963be7d866b66b29fd58b9ec18be16d062b
rutrob <faherlele@gmail.com>
2024-03-26 01:06:29 +0100

    Add category filter

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/product/ProductRepository.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/product/ProductRepository.java
@@ -11,5 +11,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE p.user.id = :userId")
     List<Product> findProductsByUserId(@Param("userId") Long userId);
 
+    @Query("SELECT p FROM Product p WHERE " +
+            "(:categoryId IS NULL OR p.category.id = :categoryId)")
+    List<Product> findProductsFiltered(@Param("categoryId") Long categoryId);
+
     List<Product> findProductsByCountGreaterThan(Integer count);
+
+    @Query("SELECT p FROM Product p WHERE (:categoryId IS NULL OR p.category.id = :categoryId) AND p.count > :count")
+    List<Product> findProductsFilteredByCountAndCategory(@Param("categoryId") Long categoryId, @Param("count") Integer count);
+
 }
