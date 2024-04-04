4241c4f7242659d230e0a56f104893e585d7de72
rutrob <faherlele@gmail.com>
2024-04-05 00:35:34 +0200

    Add filter by name

--- BuyLifyApp/src/main/java/com/buylify/buylifyapp/product/ProductRepository.java
+++ BuyLifyApp/src/main/java/com/buylify/buylifyapp/product/ProductRepository.java
@@ -16,8 +16,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE (:categoryId IS NULL OR p.category.id = :categoryId) " +
             "AND (:minPrice IS NULL OR p.price >= :minPrice) " +
             "AND (:maxPrice IS NULL OR p.price <= :maxPrice) " +
+            "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
             "AND p.count > :count AND p.isActive = true ")
     List<Product> findProductsFiltered(@Param("categoryId") Long categoryId,
+                                       @Param("name") String name,
                                        @Param("minPrice") Float minPrice,
                                        @Param("maxPrice") Float maxPrice,
                                        @Param("count") Integer count);
