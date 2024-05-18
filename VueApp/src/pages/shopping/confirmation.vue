0c833e2646276aa8323494d705b9ae2cd799596c
Mateusz Horczak <82621@student.pb.edu.pl>
2024-05-18 19:21:07 +0200

    move logos to components

--- VueApp/src/pages/shopping/confirmation.vue
+++ VueApp/src/pages/shopping/confirmation.vue
@@ -69,7 +69,7 @@ const buyProduct = async () => {
   <UProgress animation="carousel" v-if="confirmPurchase"/>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-auto w-1/2" src="~/images/logo-text.png" alt="BuyLify"/>
+      <text-logo />
       <h1 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Potwierdź swoje zamówienie</h1>
     </div>
 
