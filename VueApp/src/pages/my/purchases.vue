6ecbb7c4358bd4060b7a3cd2989c06f6f7063eb1
Mateusz Horczak <82621@student.pb.edu.pl>
2024-05-28 12:57:05 +0200

    add opinion after buy products

--- VueApp/src/pages/my/purchases.vue
+++ VueApp/src/pages/my/purchases.vue
@@ -27,18 +27,6 @@ watchEffect(() => {
     <div class="sm:mx-auto sm:w-full sm:max-w-6xl">
       <single-order v-for="order in sortedOrders" :order-id="order.orderId" :products="order.products" />
 
-<!--      <single-order v-for="order in orders"-->
-<!--                    :key="order.id"-->
-<!--                    :id="order.id"-->
-<!--                    :address="order.address"-->
-<!--                    :payment-method="order.paymentMethod"-->
-<!--                    :product="order.product"-->
-<!--                    :product-quantity="order.productQuantity"-->
-<!--                    :order-status="order.orderStatus"-->
-<!--                    :total-value="order.totalValue"-->
-<!--                    :create-at="order.createAt"-->
-<!--                    :delivery-method="order.deliveryMethod"-->
-<!--      />-->
     </div>
   </div>
 </template>
